9318456b80cc3f04d3f5ba8320ea01f09a439017
Brandon Pretelt <brandonpretelt93@gmail.com>
2022-12-05 21:54:17 -0700

    :sparkles: added Header and Footer component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<!-- <div id="app-container" class="flex justify-center">
+
+    <main class="flex justify-center items-center flex-col">
+		<slot />
+		<Footer />
+	</main>
+</div> -->
+<slot />
