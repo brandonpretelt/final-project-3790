1107997ac0916cfd50805524c03c400b67c1c924
Brandon Pretelt <brandonpretelt93@gmail.com>
2022-12-05 21:54:47 -0700

    :sparkles: installed daisyui and tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Header />
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-2xl font-bold">Final Project</h1>
+			<p>This is the entry point for my final project for DGM 3790</p>
+		</div>
+	</div>
+</div>
+<Footer />
