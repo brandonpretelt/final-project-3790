6fadf3b40f5d1935097a1ea4410c304224aa51ac
Brandon Pretelt <brandonpretelt93@gmail.com>
2022-12-07 23:33:13 -0700

    :sparkles: changed header color and working on body copy

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,28 +1,33 @@
-<div class="navbar bg-base-100">
+<div class="navbar bg-green-500">
 	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<a class="btn btn-ghost normal-case text-xl">InstaFood</a>
 	</div>
 	<div class="flex-none gap-2">
-		<div class="form-control">
-			<input type="text" placeholder="Search" class="input input-bordered" />
-		</div>
 		<div class="dropdown dropdown-end">
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<!-- svelte-ignore a11y-label-has-associated-control -->
 			<label tabindex="0" class="btn btn-ghost btn-circle avatar">
 				<div class="w-10 rounded-full">
+					<!-- svelte-ignore a11y-missing-attribute -->
 					<img src="https://placeimg.com/80/80/people" />
 				</div>
 			</label>
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 			<ul
 				tabindex="0"
 				class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
 			>
 				<li>
+					<!-- svelte-ignore a11y-missing-attribute -->
 					<a class="justify-between">
 						Profile
 						<span class="badge">New</span>
 					</a>
 				</li>
+				<!-- svelte-ignore a11y-missing-attribute -->
 				<li><a>Settings</a></li>
+				<!-- svelte-ignore a11y-missing-attribute -->
 				<li><a>Logout</a></li>
 			</ul>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
-			<h1 class="text-2xl font-bold">Final Project</h1>
-			<p>This is the entry point for my final project for DGM 3790</p>
+			<h1 class="text-3xl font-bold">InstaFood</h1>
+			<p>Your time is precious, let InstaFood handle the groceries.</p>
 		</div>
 	</div>
 </div>
