e14e6a4df0aafded8391734fcaa4db1d67a2678a
Brandon Pretelt <brandonpretelt93@gmail.com>
2022-12-12 13:04:22 -0700

    fixed btn

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -5,18 +5,18 @@
 <div class="card card-body grid grid-cols-4">
 	{#if data}
 		{#each data.products as { title, image, id }}
-			<a href={`../products/${id}`} class="box">
-				<div class="rounded-lg card-content bg-slate-700">
-					<!-- svelte-ignore a11y-missing-attribute -->
-					<figure class="p-3 w-full">
-						<img class="rounded-sm" src={image} />
-					</figure>
-					<div class="text-white text-center p-3">{title}</div>
-					<div class="flex justify-center m-3">
+			<div class="rounded-lg card-content bg-slate-700">
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<figure class="p-3 w-full">
+					<img class="rounded-sm" src={image} />
+				</figure>
+				<div class="text-white text-center p-3">{title}</div>
+				<div class="flex justify-center m-3">
+					<a href={`../products/${id}`} class="box">
 						<button class="btn bg-orange-400 mb-3">Quick Buy</button>
-					</div>
+					</a>
 				</div>
-			</a>
+			</div>
 		{/each}
 	{:else}
 		<div>welp...</div>
